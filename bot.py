c6606ed256ae7218c42c155703d2fbd732e08529
Andrey Alekseev <leksus@leksus.info>
2023-08-31 02:37:22 +0300

    new version of data storage schema

--- bot.py
+++ bot.py
@@ -1,43 +1,127 @@
 import logging
 
 from environs import Env
-from telegram import Update, ForceReply
+from telegram import Update, ForceReply, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from redis import Redis
+
+MAX_TRIES_COUNT = 3
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
-
 logger = logging.getLogger(__name__)
 
 
-def start(update: Update, context: CallbackContext) -> None:
+def start(update, redis_client):
     user = update.effective_user
+    setup_new_user_storage = {
+        'current_question': '',
+        'tries_count': 1,
+        'score': 0,
+    }
+    redis_client.hset(user.id, mapping=setup_new_user_storage)
+
+    custom_keyboard = [['Новый вопрос', 'Сдаться'],
+                       ['Мой счет']]
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
     update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
+        f'Привет, {user.mention_markdown_v2()}\! Добро пожаловать на викторину\. '
+        f'У тебя на каждый вопрос только {MAX_TRIES_COUNT} попытки\. Чтобы начать, '
+        f'нажми на кнопку "Новый вопрос"\.',
+        reply_markup=reply_markup,
     )
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
+def cancel(update, _):
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+def send_reply(update, redis_client):
+    user_id = update.effective_user.id
+    user_storage = redis_client.hgetall(user_id)
+    reply_text = ''
+    if user_storage['current_question']:
+        q_and_a = redis_client.hgetall(user_storage['current_question'])
+        if update.message.text.strip().lower() == q_and_a['clean_answer'].lower():
+            redis_client.hset(
+                user_id,
+                mapping={
+                    'current_question': '',
+                    'tries_count': 1,
+                    'score': int(user_storage['score']) + 2,
+                }
+            )
+            reply_text = 'Правильно! Твой счет +2! Для следующего вопроса нажми "Новый вопрос"'
+        elif int(user_storage['tries_count']) < MAX_TRIES_COUNT:
+
+            reply_text = f'Неправильно :( Попробуй еще раз, у тебя осталось ' \
+                         f'{MAX_TRIES_COUNT - int(user_storage["tries_count"])} попытки'
+            redis_client.hset(user_id, 'tries_count', int(user_storage['tries_count']) + 1)
+        elif int(user_storage['tries_count']) == MAX_TRIES_COUNT:
+            redis_client.hset(
+                user_id,
+                mapping={
+                    'current_question': '',
+                    'tries_count': 1,
+                    'score': int(user_storage['score']) - 2,
+                }
+            )
+            reply_text = 'У тебя не осталось попыток, твой счет -2, нажми на "Новый вопрос"'
+    elif update.message.text == 'Новый вопрос':
+        random_question_hash = redis_client.srandmember('question_hashes')
+        q_and_a = redis_client.hgetall(random_question_hash)
+        redis_client.hset(user_id, 'current_question', random_question_hash)
+        reply_text = q_and_a['question']
+    elif update.message.text == 'Сдаться':
+        q_and_a = redis_client.hgetall(user_storage['current_question'])
+        redis_client.hset(
+            user_id,
+            mapping={
+                'current_question': '',
+                'tries_count': 1,
+                'score': int(user_storage['score']) - 1,
+            }
+        )
+        reply_text = f'Твой счет понижен на 1. ' \
+                     f'Вот тебе правильный ответ: \n{q_and_a["full_answer"]}.\n\n' \
+                     'Чтобы продолжить, нажми "Новый вопрос"'
+    elif update.message.text == 'Мой счет':
+        reply_text = f'Твой счет: {user_storage["score"]}'
+    else:
+        reply_text = 'Вот сейчас не совсем понятно было. Нажми на одну из кнопок ниже.'
+
+    custom_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счет']]
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+    update.message.reply_text(reply_text, reply_markup=reply_markup)
 
 
 def run() -> None:
-
     env = Env()
     env.read_env()
+
     updater = Updater(env('TG_TOKEN'))
-    dispatcher = updater.dispatcher
+    redis_client = Redis.from_url(env('REDIS'), decode_responses=True)
 
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
+    updater.dispatcher.add_handler(
+        CommandHandler(
+            'start',
+            lambda update, _: start(update, redis_client),
+        )
+    )
+    updater.dispatcher.add_handler(
+        CommandHandler(
+            'cancel',
+            lambda update, _: start(update, redis_client),
+        )
+    )
 
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    updater.dispatcher.add_handler(
+        MessageHandler(
+            Filters.text & ~Filters.command,
+            lambda update, _: send_reply(update, redis_client)
+        )
+    )
 
     updater.start_polling()
     updater.idle()
--- load_data_to_DB.py
+++ load_data_to_DB.py
@@ -1,11 +1,10 @@
 import argparse
+import hashlib
 import logging
 import os
 
 from environs import Env
 from redis import Redis
-
-LAST_QUESTION_ID = 0
 
 logger = logging.getLogger(__name__)
 
@@ -44,12 +43,17 @@ def parse_file_content(file_content):
     return q_and_a
 
 
+def get_cleaned_answer(answer):
+    return min(answer.split('.')[0], answer.split('(')[0], key=len)
+
+
 def run():
     handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter('%(message)s'))
+    handler.setFormatter(
+        logging.Formatter(
+            '%(asctime)s %(message)s'))
     logger.setLevel(logging.INFO)
     logger.addHandler(handler)
-
     logger.info('Start parsing...')
 
     env = Env()
@@ -60,23 +64,38 @@ def run():
     redis_client = Redis.from_url(env('REDIS'), decode_responses=True)
     logger.info('Connected to redis')
 
-    question_id = LAST_QUESTION_ID
     filepaths = []
     for root, _, filenames in os.walk(args.path, topdown=False):
-        filepaths = (os.path.join(root, file) for file in filenames)
+        filepaths = [os.path.join(root, file) for file in filenames]
 
-    logger.info(f'Found {len(filepaths)} .txt files')
+    logger.info(f'Found {len(filepaths)} txt files')
 
     for filepath in filepaths:
+        count_added = 0
         with open(filepath, 'r', encoding='koi8-r') as f:
             file_content = f.read()
         q_and_a_pairs = parse_file_content(file_content)
-        logger.info(f'Found {len(q_and_a_pairs)} q&a in {filepath} file, load it to redis')
-        for q_and_a_pair in q_and_a_pairs:
-            redis_client.hmset(f'q{question_id}', q_and_a_pair)
-            question_id += 1
 
-        logger.info(f'Loading from {filepath} file complete, taking another file')
+        logger.info(f'Found {len(q_and_a_pairs)} q&a in {filepath} file, load it to redis')
+
+        for q_and_a_pair in q_and_a_pairs:
+
+            q_hash = hashlib.md5(q_and_a_pair['question'].encode()).hexdigest()
+            if not redis_client.sismember('question_hashes', q_hash):
+                redis_client.sadd('question_hashes', q_hash)
+                redis_client.hset(
+                    q_hash,
+                    mapping={
+                        'question': q_and_a_pair['question'],
+                        'full_answer': q_and_a_pair['answer'],
+                        'clean_answer': get_cleaned_answer(q_and_a_pair['answer']),
+                    },
+                )
+                count_added += 1
+
+        logger.info(f'Loading from {filepath} file complete, added {count_added} q&a')
+
+    logger.info(f'Loading to DB complete')
 
 
 if __name__ == '__main__':
