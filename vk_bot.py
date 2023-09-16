38e85c031100aafc36c8c27476883e8d943d8f5f
Andrey Alekseev <leksus@leksus.info>
2023-09-16 19:34:39 +0300

    refactor: split func

--- vk_bot.py
+++ vk_bot.py
@@ -20,55 +20,90 @@ def draw_keyboard():
     return keyboard.get_keyboard()
 
 
+def set_welcome_state(event, redis_client):
+    setup_new_user_storage = {
+        'current_question_hash': '',
+        'score': 0,
+    }
+    redis_client.hset(event.user_id, mapping=setup_new_user_storage)
+    reply_text = f'Привет! Добро пожаловать на викторину. ' \
+                 f'Нажми на кнопку "Новый вопрос". ' \
+                 f'Для отмены игры набери /cancel',
+
+    logger.info(f'Новый пользователь {event.user_id} вступил в игру')
+
+    return reply_text
+
+
+def ask_question_state(event, redis_client):
+    random_question_hash = redis_client.srandmember('question_hashes')
+    redis_client.hset(event.user_id, 'current_question_hash', random_question_hash)
+    q_and_a = redis_client.hgetall(random_question_hash)
+    reply_text = q_and_a['question']
+
+    logger.debug(f'Пользователю {event.user_id} задан вопрос с id {random_question_hash}')
+
+    return reply_text
+
+
+def surrender_state(event, redis_client, user_storage):
+    q_and_a = redis_client.hgetall(user_storage['current_question_hash'])
+    redis_client.hset(event.user_id, 'current_question_hash', '')
+    reply_text = f'Вот тебе правильный ответ: {q_and_a["full_answer"]}\n' \
+                 'Чтобы продолжить, нажми "Новый вопрос"'
+
+    logger.debug(
+        f'Пользователь {event.user_id} сдался, не ответив на вопрос c id {user_storage["current_question_hash"]}')
+
+    return reply_text
+
+
+def cancel_quiz_state(event, redis_client, user_storage):
+    reply_text = f'Спасибо за игру! Вы заработали {user_storage["score"]} очков.'
+    redis_client.delete(event.user_id)
+
+    logger.info(f'Пользователь {event.user_id} покинул игру, заработав {user_storage["score"]} очков')
+
+    return reply_text
+
+
+def check_answer_state(event, redis_client, user_storage):
+    q_and_a = redis_client.hgetall(user_storage['current_question_hash'])
+    if event.text.strip().lower() == q_and_a['clean_answer']:
+        redis_client.hset(event.user_id, 'score', int(user_storage['score']) + 1)
+        redis_client.hset(event.user_id, 'current_question_hash', '')
+        reply_text = 'Правильно! Счет увеличен! Для следующего вопроса нажми "Новый вопрос"'
+        logger.debug(f'Пользователь {event.user_id} ответил верно '
+                     f'на вопрос с id {user_storage["current_question_hash"]}')
+    else:
+        reply_text = 'Неверно, попробуй еще раз'
+        logger.debug(f'Пользователь {event.user_id} ответил неверно '
+                     f'на вопрос с id {user_storage["current_question_hash"]}')
+    return reply_text
+
+
 def send_reply(event, bot, redis_client):
     user_storage = redis_client.hgetall(event.user_id)
 
     if not user_storage:
-        setup_new_user_storage = {
-            'current_question_hash': '',
-            'score': 0,
-        }
-        redis_client.hset(event.user_id, mapping=setup_new_user_storage)
-        reply_text = f'Привет! Добро пожаловать на викторину. ' \
-        f'Нажми на кнопку "Новый вопрос". ' \
-        f'Для отмены игры набери /cancel',
-        logger.info(f'Новый пользователь {event.user_id} вступил в игру')
+        reply_text = set_welcome_state(event, redis_client)
 
     elif event.text == 'Новый вопрос' and not user_storage['current_question_hash']:
-        random_question_hash = redis_client.srandmember('question_hashes')
-        redis_client.hset(event.user_id, 'current_question_hash', random_question_hash)
-        q_and_a = redis_client.hgetall(random_question_hash)
-        reply_text = q_and_a['question']
-        logger.debug(f'Пользователю {event.user_id} задан вопрос с id {random_question_hash}')
+        reply_text = ask_question_state(event, redis_client)
 
     elif event.text == 'Сдаться' and user_storage['current_question_hash']:
-        q_and_a = redis_client.hgetall(user_storage['current_question_hash'])
-        redis_client.hset(event.user_id, 'current_question_hash', '')
-        reply_text = f'Вот тебе правильный ответ: {q_and_a["full_answer"]}\n' \
-                     'Чтобы продолжить, нажми "Новый вопрос"'
-        logger.debug(f'Пользователь {event.user_id} сдался, не ответив на вопрос c id {user_storage["current_question_hash"]}')
+        reply_text = surrender_state(event, redis_client, user_storage)
 
     elif event.text == '/cancel':
-        reply_text = f'Спасибо за игру! Вы заработали {user_storage["score"]} очков.'
-        redis_client.delete(event.user_id)
-        logger.info(f'Пользователь {event.user_id} покинул игру, заработав {user_storage["score"]} очков')
+        reply_text = cancel_quiz_state(event, redis_client, user_storage)
 
     elif event.text == 'Мой счет':
         reply_text = f'Твой счет: {user_storage["score"]} очков.'
         logger.debug(f'Пользователь {event.user_id} запросил свой счет')
 
     elif user_storage['current_question_hash']:
-        q_and_a = redis_client.hgetall(user_storage['current_question_hash'])
-        if event.text.strip().lower() == q_and_a['clean_answer']:
-            redis_client.hset(event.user_id, 'score', int(user_storage['score']) + 1)
-            redis_client.hset(event.user_id, 'current_question_hash', '')
-            reply_text = 'Правильно! Счет увеличен! Для следующего вопроса нажми "Новый вопрос"'
-            logger.debug(f'Пользователь {event.user_id} ответил верно '
-                         f'на вопрос с id {user_storage["current_question_hash"]}')
-        else:
-            reply_text = 'Неверно, попробуй еще раз'
-            logger.debug(f'Пользователь {event.user_id} ответил неверно '
-                         f'на вопрос с id {user_storage["current_question_hash"]}')
+        reply_text = check_answer_state(event, redis_client, user_storage)
+
     else:
         reply_text = f'Вот сейчас не понял тебя :('
         logger.debug(f'Пользователь ответил не по сценарию')
